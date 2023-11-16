9cd1277283de9c97de3ed0a932cda2600928bc68
CyanFaux <santafox26@gmail.com>
2023-11-15 20:58:26 -0500

    "finishing touches"

--- index.js
+++ index.js
@@ -26,30 +26,3 @@ newImage('assets/well.png', 'Well', '500px', '425px')
 newItem('assets/sword.png', '500px', '405px')
 newItem('assets/shield.png', '165px', '185px')
 newItem('assets/staff.png', '600px', '100px')
-
-const body
-/* let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter) */
-
-/* let pineTree = document.createElement("img");
-pineTree.src = "assets/pine-tree.png";
-pineTree.style.position = "fixed";
-pineTree.style.left = "450px";
-pineTree.style.bottom = "200px";
-document.body.append(pineTree); */
-
-
-/* function newItem (src, left, bottom) {
-    let itemElement = document.createElement('img');
-itemElement.src = src;
-itemElement.style.position = 'fixed';
-itemElement.style.left = left || '0';
-itemElement.style.bottom = bottom || '0';
-document.body.append(itemElement);
-
-
-} */
